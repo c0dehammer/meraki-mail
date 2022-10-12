12841f7fb03fea685640b7b5edf93ad712b25d40
c0dehammer <50229753+c0dehammer@users.noreply.github.com>
2022-10-12 13:08:10 +0530

    Update pre-install.sh

--- pre-install.sh
+++ pre-install.sh
@@ -6,5 +6,6 @@ apt-get install software-properties-common git -y
 add-apt-repository ppa:deadsnakes/ppa
 apt install python3 -y
 apt install python3-virtualenv -y
+git clone https://github.com/modoboa/modoboa-installer
 mkdir -p /etc/cron.d/
 
