fdcda2870554ab73ffa5af0b89bcc6ef360a6de7
c0dehammer <50229753+c0dehammer@users.noreply.github.com>
2022-10-12 18:44:27 +0530

    Update pre-install.sh

--- pre-install.sh
+++ pre-install.sh
@@ -5,6 +5,7 @@ apt-get upgrade -y
 apt-get install software-properties-common git -y
 add-apt-repository ppa:deadsnakes/ppa
 apt install python3 -y
+apt install python3-pip -y
 apt install python3-virtualenv -y
 rm -rf ./modoboa-installer
 git clone https://github.com/modoboa/modoboa-installer
