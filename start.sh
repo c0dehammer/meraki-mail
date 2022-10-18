8a137784c5dc7c6eaf3aacaa18dfc2cfe10296c3
c0dehammer <50229753+c0dehammer@users.noreply.github.com>
2022-10-19 01:00:30 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -11,7 +11,7 @@ CONTAINER_FIRST_STARTUP="CONTAINER_FIRST_STARTUP"
 if [ ! -e /$CONTAINER_FIRST_STARTUP ]; then
     touch /$CONTAINER_FIRST_STARTUP
     # place your script that you only want to run on first startup.
-    modoboa_installer/run.py --debug --force ${DOMAIN}
+    modoboa-installer/run.py --debug --force ${DOMAIN}
 else
     # script that should run the rest of the times (instances where you 
     # stop/restart containers).
