65c83b6deb741d368bbc2d316e5a308e830eb8b5
c0dehammer <50229753+c0dehammer@users.noreply.github.com>
2022-10-23 20:15:21 +0530

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+PYTHON_VERSION = python3 -c 'import sys; print(".".join(map(str, sys.version_info[0:2])))'
+
 sed -i '0,/pidfile=/ s/pidfile=/pidfile=\$PIDFILE/' /etc/init.d/opendkim
 sed -i '0,/dnsbl.sorbs.net/ s/dnsbl.sorbs.net/#dnsbl.sorbs.net/' /etc/postfix/main.cf
 sed -i '0,/postscreen_bare_newline_enable = yes/ s/postscreen_bare_newline_enable = yes/postscreen_bare_newline_enable = no/' /etc/postfix/main.cf
