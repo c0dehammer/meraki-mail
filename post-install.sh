499b8b52f86cda0dea2a794d504adfa194a93076
c0dehammer <50229753+c0dehammer@users.noreply.github.com>
2022-10-23 19:51:07 +0530

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -6,3 +6,5 @@ sed -i '0,/postscreen_bare_newline_enable = yes/ s/postscreen_bare_newline_enabl
 sed -i '0,/postscreen_non_smtp_command_enable = yes/ s/postscreen_non_smtp_command_enable = yes/postscreen_non_smtp_command_enable = no/' /etc/postfix/main.cf
 sed -i '0,/postscreen_pipelining_enable = yes/ s/postscreen_pipelining_enable = yes/postscreen_pipelining_enable = no/' /etc/postfix/main.cf
 sed -i '0,/mail.localhost/ s/mail.localhost/\*/' /srv/modoboa/instance/instance/settings.py
+# caldav issue https://github.com/modoboa/modoboa/issues/2571#issuecomment-1210003862
+sed -i 's/username=username, password=password)/username=username, password=password, ssl_verify_cert=False)/' /srv/modoboa/env/lib/python3.10/site-packages/modoboa_radicale/backends/caldav_.py
