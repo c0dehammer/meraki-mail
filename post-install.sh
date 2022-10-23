9fc066b4cd54981fea1d209d3815560a48a5fefd
c0dehammer <50229753+c0dehammer@users.noreply.github.com>
2022-10-23 20:29:54 +0530

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-PYTHON_VERSION = python3 -c 'import sys; print(".".join(map(str, sys.version_info[0:2])))'
+PYTHON_VERSION=$(python3 -c 'import sys; print(".".join(map(str, sys.version_info[0:2])))')
+PYTHON_VERSION="python"$PYTHON_VERSION
 
 sed -i '0,/pidfile=/ s/pidfile=/pidfile=\$PIDFILE/' /etc/init.d/opendkim
 sed -i '0,/dnsbl.sorbs.net/ s/dnsbl.sorbs.net/#dnsbl.sorbs.net/' /etc/postfix/main.cf
@@ -9,4 +10,4 @@ sed -i '0,/postscreen_non_smtp_command_enable = yes/ s/postscreen_non_smtp_comma
 sed -i '0,/postscreen_pipelining_enable = yes/ s/postscreen_pipelining_enable = yes/postscreen_pipelining_enable = no/' /etc/postfix/main.cf
 sed -i '0,/mail.localhost/ s/mail.localhost/\*/' /srv/modoboa/instance/instance/settings.py
 # caldav issue https://github.com/modoboa/modoboa/issues/2571#issuecomment-1210003862
-sed -i 's/username=username, password=password)/username=username, password=password, ssl_verify_cert=False)/' /srv/modoboa/env/lib/python3.10/site-packages/modoboa_radicale/backends/caldav_.py
+sed -i 's/username=username, password=password)/username=username, password=password, ssl_verify_cert=False)/' /srv/modoboa/env/lib/$PYTHON_VERSION/site-packages/modoboa_radicale/backends/caldav_.py
