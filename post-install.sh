8105b642bb64123dc7c04d43d87c9ce6312908d1
c0dehammer <50229753+c0dehammer@users.noreply.github.com>
2022-10-26 12:57:14 +0530

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -9,12 +9,23 @@ sed -i '0,/postscreen_bare_newline_enable = yes/ s/postscreen_bare_newline_enabl
 sed -i '0,/postscreen_non_smtp_command_enable = yes/ s/postscreen_non_smtp_command_enable = yes/postscreen_non_smtp_command_enable = no/' /etc/postfix/main.cf
 sed -i '0,/postscreen_pipelining_enable = yes/ s/postscreen_pipelining_enable = yes/postscreen_pipelining_enable = no/' /etc/postfix/main.cf
 sed -i '0,/mail.localhost/ s/mail.localhost/\*/' /srv/modoboa/instance/instance/settings.py
+
 # caldav issue https://github.com/modoboa/modoboa/issues/2571#issuecomment-1210003862
 sed -i 's/username=username, password=password)/username=username, password=password, ssl_verify_cert=False)/' /srv/modoboa/env/lib/$PYTHON_VERSION/site-packages/modoboa_radicale/backends/caldav_.py
-# emails not being sent or received
-sed -i 's/check_policy_service/# check_policy_service/' /etc/postfix/main.cf
-# manage.py not working
-export PYTHONPATH=/srv/modoboa/env/lib/$PYTHON_VERSION/site-packages
 
+# manage.py not working
+#  adding path
+export PYTHONPATH=/srv/modoboa/env/lib/$PYTHON_VERSION/site-packages
+#  changing interpreter to python3 
 sed -i 's/env python/env python3/' /srv/modoboa/instance/manage.py
+
+
+# emails not being sent or received
+#  this was due to policy service not starting. either remove the dependency of policy service from postfix, such as below. 
+sed -i 's/check_policy_service/# check_policy_service/' /etc/postfix/main.cf
+#  or remove ipv6 references which prevent policy service from starting
+sed -i 's/\::1/# else policy service does not start ::1/' /etc/hosts
+
+
+
 echo "** post-install.sh has finished running..."
